d34022f173b18294fcb53619005577225acd5667
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-01 20:40:44 +0100

    Create basics of views, render app in MainLayout

---
+++ src/components/views/Waiter/Waiter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Waiter.scss';
+
+const Waiter = () => (
+  <div className={styles.component}>
+    <h2>Waiter View</h2>
+  </div>
+);
+
+export default Waiter;
