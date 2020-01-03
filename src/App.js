bd69bb3196cdde53581f5ba896a5045e1d36f418
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-03 10:45:06 +0100

    Introduce Material-UI, style nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Dashboard from './components/views/Dashboard/Dashboard';
@@ -8,34 +11,51 @@ import Tables from './components/views/Tables/Tables';
 import Waiter from './components/views/Waiter/Waiter';
 import Kitchen from './components/views/Kitchen/Kitchen';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#212121',
+    },
+    /*     secondary: {
+      main: '#11cb5f',
+    }, */
+  },
+});
+
 function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}/>
-          <Route path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
-          <Route path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
-          <Route path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter}/>
-          <Route path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen}/>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <MainLayout>
+            <Switch>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}/>
+              <Route path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
+              <Route path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
+              <Route path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter}/>
+              <Route path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen}/>
 
-          <div className="App">
-            <header className="App-header">
-              <p>
+              <div className="App">
+                <header className="App-header">
+                  <p>
           Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+                  </p>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
           Learn React
-              </a>
-            </header>
-          </div>
-        </Switch>
-      </MainLayout>
+                  </a>
+                </header>
+              </div>
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+
+      </StylesProvider>
+
     </BrowserRouter>
 
   );
--- src/components/layout/PageNav/PageNav.test.js
+++ src/components/layout/PageNav/PageNav.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import PageNav from './PageNav';
+import Button from '@material-ui/core/Button';
 
 describe('Component PageNav', () => {
   it('renders without crashing', () => {
     expect(shallow(<PageNav />)).toBeTruthy();
   });
-  it('renders NavLinks', () => {
+  it('renders Buttons', () => {
     const wrapper = shallow(<PageNav />);
-    expect(wrapper.find('NavLink').length).toEqual(5);
+    console.debug(wrapper);
+    expect(wrapper.find(Button).length).toEqual(5);
   });
 });
