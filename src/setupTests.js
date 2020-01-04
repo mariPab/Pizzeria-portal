5b39abf2c7996f7e5f635072dc1d3faf80c874f9
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-04 15:40:19 +0100

    Configure tests settings to test whether app throws errors

--- src/setupTests.js
+++ src/setupTests.js
@@ -7,4 +7,8 @@ import '@testing-library/jest-dom/extend-expect';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+console.error = err => {
+  throw new Error(err);
+};
+
 configure({ adapter: new Adapter() });
