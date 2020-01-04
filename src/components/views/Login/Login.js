047a51e5f48f9799a1b29b57891521c9c72093f8
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-04 13:03:52 +0100

    Add login panel

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,10 +1,36 @@
 import React from 'react';
-
-import styles from './Login.scss';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import styles from './Login.module.scss';
+import {NavLink} from 'react-router-dom';
 
 const Login = () => (
   <div className={styles.component}>
-    <h2>Login View</h2>
+    <h2>Login</h2>
+    <Container maxWidth='lg'>
+      <form className={styles.form} noValidate autoComplete='off'>
+        <TextField
+          className={styles.input}
+          id="outlined-secondary"
+          label="Login"
+          variant="outlined"
+          color="secondary"
+        />
+        <TextField
+          className={styles.input}
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+          color="secondary"
+        />
+        <Button className={styles.button} size='large' variant="contained" color="secondary" component={NavLink} exact to={`${process.env.PUBLIC_URL}/`}>
+          Login
+        </Button>
+      </form>
+    </Container>
   </div>
 );
 
