84e58553bcca59a6f4daa309fdf32d7d71af35ad
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-07 18:33:46 +0100

    Add table with active orders in KItchen view

--- src/components/views/Kitchen/Kitchen.js
+++ src/components/views/Kitchen/Kitchen.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import styles from './Kitchen.module.scss';
 
-import styles from './Kitchen.scss';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Fab from '@material-ui/core/Fab';
+import DoneIcon from '@material-ui/icons/Done';
+
+const demoContent = [
+  {id: '23', table: '1', details: 'Pizza, Sauce: Tomato, Topings: cucumber'},
+  {id: '24', table: '4', details: 'Espresso'},
+  {id: '25', table: '6', details: 'Salad, Topings: Basil'},
+  {id: '26', table: '3', details: 'Pizza, Sauce: Tomato, Topings: cucumber, ham, mushrooms'},
+  {id: '27', table: 'N/A', details: '2x Pizza, Sauce: Tomato, Topings: ham, mushrooms'},
+  {id: '28', table: 'N/A', details: 'Caffe Latte, Espresso, 2x Donut'},
+];
 
 const Kitchen = () => (
   <div className={styles.component}>
-    <h2>Kitchen View</h2>
+    <h2>Active Orders</h2>
+    <Paper >
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Order ID</TableCell>
+            <TableCell>Table</TableCell>
+            <TableCell>Order Details</TableCell>
+            <TableCell>Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {demoContent.map(row => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell>
+                {row.table}
+              </TableCell>
+              <TableCell>
+                {row.details}
+              </TableCell>
+              <TableCell>
+                <Fab size="small" color='secondary' aria-label='add'>
+                  <DoneIcon />
+                </Fab>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   </div>
 );
 
--- src/components/views/Kitchen/Kitchen.test.js
+++ src/components/views/Kitchen/Kitchen.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Kitchen from './Kitchen';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
 
 describe('Component Kitchen', () => {
   it('renders without crashing', () => {
     expect(shallow(<Kitchen />)).toBeTruthy();
+  });
+  it('renders correct classNames', () => {
+    const component = shallow(<Kitchen />);
+    expect(component.hasClass('component')).toBe(true);
+  });
+  it('renders Table elements', () => {
+    const component = shallow(<Kitchen />);
+    expect(component.find(Table).length).toBe(1);
+    expect(component.find(TableHead).length).toBe(1);
+    expect(component.find(TableRow).length).toBe(7);
+    expect(component.find(TableCell).length).toBe(28);
   });
 });
