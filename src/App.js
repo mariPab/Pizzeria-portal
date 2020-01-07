194b38b6cf0cea324ea20ddc8fb3c157d63c2ab4
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-07 20:47:12 +0100

    Initialize redux store

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import Tables from './components/views/Tables/Tables';
 import Waiter from './components/views/Waiter/Waiter';
 import Kitchen from './components/views/Kitchen/Kitchen';
 
+import {Provider} from 'react-redux';
+import store from './redux/store';
+
 const theme = createMuiTheme({
   palette: {
     primary: {
@@ -24,40 +27,39 @@ const theme = createMuiTheme({
 
 function App() {
   return (
-    <BrowserRouter>
-      <StylesProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <MainLayout>
-            <Switch>
-              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}/>
-              <Route path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
-              <Route path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
-              <Route path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter}/>
-              <Route path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen}/>
+    <Provider store={store}>
+      <BrowserRouter>
+        <StylesProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <MainLayout>
+              <Switch>
+                <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}/>
+                <Route path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
+                <Route path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
+                <Route path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter}/>
+                <Route path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen}/>
 
-              <div className="App">
-                <header className="App-header">
-                  <p>
+                <div className="App">
+                  <header className="App-header">
+                    <p>
           Edit <code>src/App.js</code> and save to reload.
-                  </p>
-                  <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
+                    </p>
+                    <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
           Learn React
-                  </a>
-                </header>
-              </div>
-            </Switch>
-          </MainLayout>
-        </ThemeProvider>
-
-      </StylesProvider>
-
-    </BrowserRouter>
-
+                    </a>
+                  </header>
+                </div>
+              </Switch>
+            </MainLayout>
+          </ThemeProvider>
+        </StylesProvider>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
