88c917f33e41f606aa6b1da78d11a55592542170
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-07 19:37:41 +0100

    Use Material UI components to create tables on Dashboard view

--- src/components/views/Dashboard/Dashboard.js
+++ src/components/views/Dashboard/Dashboard.js
@@ -1,49 +1,88 @@
 import React from 'react';
-import styles from './Dashboard.scss';
-import CustomTable from '../../common/CustomTable/CustomTable';
+import styles from './Dashboard.module.scss';
 
-const orderStatisticsColumns = [
-  {title: 'Order ID', field: 'orderId'},
-  {title: 'Order Value', field: 'value'},
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const demoContentBookings = [
+  {id: '1', name: 'Anna', phone: '465987584', hours: '14:00 - 17:00', table: 2},
+  {id: '2', name: 'Robert', phone: '786789678', hours: '12:30 - 13:00', table: 1},
+  {id: '3', name: 'John', phone: '786780453', hours: '16:30 - 19:00', table: 1},
+  {id: '4', name: 'Dean', phone: '567456876', hours: '17:00 - 21:00', table: 2},
+  {id: '5', name: 'Tom', phone: '567531231', hours: '22:00 - 24:00', table: 3},
+  {id: '6', name: 'Robert', phone: '786789678', hours: '12:30 - 13:00', table: 2},
 ];
 
-const orderStatisticsData = [
-  {orderId: 2, value: 55},
-  {orderId: 3, value: 130},
-  {orderId: 4, value: 80},
-  {orderId: 5, value: 45},
-];
-
-const tableBookingColumns = [
-  {title: 'Name', field: 'name'},
-  {title: 'Phone', field: 'phone'},
-  {title: 'Hours', field: 'hours'},
-  {title: 'Table No.', field: 'table'},
-];
-
-const tableBookingData = [
-  {name: 'Anna', phone: '465987584', hours: '14:00 - 17:00', table: 2},
-  {name: 'Robert', phone: '786789678', hours: '12:30 - 13:00', table: 1},
-  {name: 'John', phone: '786780453', hours: '16:30 - 19:00', table: 1},
-  {name: 'Dean', phone: '567456876', hours: '17:00 - 21:00', table: 2},
-  {name: 'Tom', phone: '567531231', hours: '22:00 - 24:00', table: 3},
-  {name: 'Robert', phone: '786789678', hours: '12:30 - 13:00', table: 2},
+const demoContentStatistic = [
+  {id: '1', value: 452},
+  {id: '2', value: 345},
+  {id: '3', value: 123},
+  {id: '4', value: 234},
+  {id: '5', value: 345},
+  {id: '6', value: 456},
 ];
 
 const Dashboard = () => (
   <div className={styles.component}>
     <h2>Dashboard</h2>
-    <CustomTable
-      columns={orderStatisticsColumns}
-      data={orderStatisticsData}
-      title={'Order Statistics'}
-      editable={false}
-    />
-    <CustomTable
-      columns={tableBookingColumns}
-      data={tableBookingData}
-      title={'Table booking'}
-      editable={false} />
+    <h3>Order Statistics</h3>
+    <Paper >
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Order ID</TableCell>
+            <TableCell>Order Value</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {demoContentStatistic.map(row => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell>
+                {row.value}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+    <h3>Table bookings for today</h3>
+    <Paper >
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Phone</TableCell>
+            <TableCell>Hour</TableCell>
+            <TableCell>Table</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {demoContentBookings.map(row => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell>
+                {row.phone}
+              </TableCell>
+              <TableCell>
+                {row.hours}
+              </TableCell>
+              <TableCell>
+                {row.table}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   </div>
 );
 
--- src/components/views/Dashboard/Dashboard.test.js
+++ src/components/views/Dashboard/Dashboard.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Dashboard from './Dashboard';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
 
 describe('Component Dashboard', () => {
   it('renders without crashing', () => {
     expect(shallow(<Dashboard />)).toBeTruthy();
   });
-  it('renders CustomTable', () => {
-    expect(shallow(<Dashboard />).find('CustomTable').length).toEqual(2);
+  it('renders Table elements', () => {
+    const component = shallow(<Dashboard />);
+    expect(component.find(Table).length).toBe(2);
+    expect(component.find(TableHead).length).toBe(2);
+    expect(component.find(TableRow).length).toBe(14);
+    expect(component.find(TableCell).length).toBe(42);
   });
 });
